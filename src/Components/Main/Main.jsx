40e63c2e22cc1d6d944141d9f1d59143a9dbe04b
Danichp <danichp01@mail.ru>
2022-12-09 15:25:18 +0700

    second commit

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -18,6 +18,7 @@ const Main = function(){
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/LCti_Ais2RA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
             <Agitation />
+            <p>lssdlfns</p>
         </div>
 
     )
